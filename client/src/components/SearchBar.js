0d9a2dff48fee636a665e0f09b2ea83f2fa7e3b8
yaelshtaiman <yaelshtaiman@gmail.com>
2025-08-11 21:47:57 +0300

    search bar - start (not working)

--- client/src/components/SearchBar.js
+++ client/src/components/SearchBar.js
@@ -1,11 +1,33 @@
-import React from "react"
+import React, {useState} from "react"
+import "./SearchBar.css"
 
-const SearchBar = () => {
+const SearchBar = ({mentors, onResults}) => {
+    const [query, setQuery] = useState("");
+
+    const handleChange = (e) => {
+        const value = e.target.value.toLowerCase();
+        setQuery(value);
+        const filtered = mentors.filter((mentor) => {
+            const fullName = (mentor.first_name + " " + mentor.last_name).toLowerCase();
+            const languages = mentor.languages.map((l) => l.toLowerCase());
+            return fullName.include(value) || languages.some((l) => l.includes(value));
+        })
+        onResults(filtered);
+    }
+
     return (
         <div>
-            Search Bar
+            <input
+                className = "search-bar"
+                type="search"
+                value={query}
+                onChange={handleChange}
+                placeholder="Search by name or language"
+            />
         </div>
-    )
+
+
+)
 }
 
 export default SearchBar;
