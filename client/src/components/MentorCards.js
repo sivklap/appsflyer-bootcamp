57b0bd4217d58db4fc4367deaebd8483c38737b1
yaelshtaiman <yaelshtaiman@gmail.com>
2025-08-11 21:04:48 +0300

    add state for chosen mentor and adding functionality for this

--- client/src/components/MentorCards.js
+++ client/src/components/MentorCards.js
@@ -1,22 +1,25 @@
 import React, {useState} from "react"
 import MentorModal from "./MentorModal.js"
 import "./MentorCards.css"
-import Avatar from '@mui/material/Avatar';
 
 const MentorCards = ({mentors}) => {
     const [showMentorModal, setShowMentorModal] = useState(false);
+    const [chosenMentor, setChosenMentor] = useState(null);
     return (
         <div className="all-mentor-cards">
             {
                 mentors.map((mentor) => {
                     return (
                         <>
-                            <div className="mentor-card" onClick={() => setShowMentorModal(true)}>
+                            <div className="mentor-card" onClick={() => {
+                                setShowMentorModal(true)
+                                setChosenMentor(mentor)
+                            }}>
                                 <img src={mentor.img} alt="mentor-image" />
                                 <h2>{mentor.first_name + " " + mentor.last_name}</h2>
                                 <p>{mentor.languages.join(", ")}</p>
                             </div>
-                            <MentorModal show={showMentorModal} mentor={mentor} onClose={() => setShowMentorModal(false)}/>
+                            <MentorModal show={showMentorModal} mentor={chosenMentor} onClose={() => setShowMentorModal(false)}/>
                         </>
                     )
                 })
