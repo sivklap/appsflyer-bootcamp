bf1353aaaa7055fdd894bc3cc482c98b48fb7526
yaelshtaiman <yaelshtaiman@gmail.com>
2025-08-12 14:57:48 +0300

    adding css file to app.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import HomePage from "./components/HomePage";
 import NavBar from "./components/NavBar";
-
+import "./App.css"
 
 function App() {
     return (
