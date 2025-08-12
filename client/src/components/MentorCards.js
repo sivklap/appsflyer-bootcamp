2163c648e6fc14b52842e70121e280edc46b60c5
yaelshtaiman <yaelshtaiman@gmail.com>
2025-08-12 20:03:22 +0300

    using the DB for fetching the mentors in mentor page (card and modal)

--- client/src/components/MentorCards.js
+++ client/src/components/MentorCards.js
@@ -5,26 +5,33 @@ import "./MentorCards.css"
 const MentorCards = ({mentors}) => {
     const [showMentorModal, setShowMentorModal] = useState(false);
     const [chosenMentor, setChosenMentor] = useState(null);
+    // console.log(mentors)
     return (
-        <div className="all-mentor-cards">
-            {
-                mentors.map((mentor) => {
-                    return (
-                        <>
-                            <div className="mentor-card" onClick={() => {
-                                setShowMentorModal(true)
-                                setChosenMentor(mentor)
-                            }}>
-                                <img src={mentor.img} alt="mentor-image" />
-                                <h2>{mentor.first_name + " " + mentor.last_name}</h2>
-                                <p>{mentor.languages.join(", ")}</p>
-                            </div>
-                            <MentorModal show={showMentorModal} mentor={chosenMentor} onClose={() => setShowMentorModal(false)}/>
-                        </>
-                    )
-                })
-            }
-        </div>
+        <>
+            <div className="all-mentor-cards">
+                {mentors.map((mentor) => (
+                    <div
+                        className="mentor-card"
+                        key={mentor._id}
+                        onClick={() => {
+                            setShowMentorModal(true);
+                            setChosenMentor(mentor);
+                        }}
+                    >
+                        <img src={mentor.imageUrl} alt="mentor-image" />
+                        <h2>{mentor.firstName} {mentor.lastName}</h2>
+                        <p>{mentor.codingLanguages.join(", ")}</p>
+                    </div>
+                ))}
+            </div>
+
+            {/* Single modal instance outside the map */}
+            <MentorModal
+                show={showMentorModal}
+                mentor={chosenMentor}
+                onClose={() => setShowMentorModal(false)}
+            />
+        </>
     )
 }
 
