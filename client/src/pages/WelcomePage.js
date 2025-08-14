fee7b56ce040675b31504d41c70250f453f41877
yaelshtaiman <yaelshtaiman@gmail.com>
2025-08-14 18:25:17 +0300

    handle the welcome page for when a user is logged in and when its a guest

--- client/src/pages/WelcomePage.js
+++ client/src/pages/WelcomePage.js
@@ -2,20 +2,32 @@ import React from "react"
 import Button from "@mui/material/Button";
 import "./WelcomePage.css"
 
-const WelcomePage = () => {
+const WelcomePage = ({user}) => {
     return (
         <div className="welcome-page">
             <img src="/images/crown-logo.png" alt="Home Logo" className="crown-logo" />
             <h3>Connecting women to learn and grow together</h3>
             <p>Join a community, find mentors and grow your skills.</p>
-            //TODO: make the button seen only if a user is not connected
-            <Button
-                className="welcome-log-in"
-                variant="contained"
-                href="/login"
-            >
-                Get started
-            </Button>
+
+            {!user ? (
+                <Button
+                    className="welcome-log-in"
+                    variant="contained"
+                    href="/login"
+                >
+                    Get started
+                </Button>
+            ) : (
+                <Button
+                    className="welcome-log-in"
+                    variant="contained"
+                    href={`/profile/${user.role}`}
+                >
+                    Go to Profile
+                </Button>
+            )}
+
+
         </div>
     )
 }
