9bb57f2187973f90577e6b7913b98d7f03aa74a8
yaelshtaiman <yaelshtaiman@gmail.com>
2025-08-14 17:40:02 +0300

    adding navbar logged-in user functionality

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,9 +1,22 @@
-import React from "react"
+import React, {useState} from "react"
 import "./NavBar.css"
 import Button from '@mui/material/Button';
+import Avatar from '@mui/material/Avatar';
+import Menu from '@mui/material/Menu';
+import MenuItem from '@mui/material/MenuItem';
+
 
 
 const NavBar = ({user}) => {
+    const [anchorEl, setAnchorEl] = useState(null);
+    const open = Boolean(anchorEl);
+    const handleClick = (e) => {
+        setAnchorEl(e.currentTarget);
+    }
+    const handleClose = () => {
+        setAnchorEl(null);
+    }
+
     return (
         <nav className="navbar">
             <a className="title" href={'/'}>
@@ -11,19 +24,29 @@ const NavBar = ({user}) => {
             </a>
             <div className="navbar-user">
                 { user ? (
-
-                    <div className="user-bye">
-                        <Button
-                            className="log-out"
-                            variant="contained"
-                            href="/"
-                        >
-                            Log out
-                        </Button>
-                    </div>
+                        <div className="navbar-logged-in">
+                            <Avatar
+                                alt={user.first_name}
+                                src={`images/avatars/avatar-${user.img}.png`}
+                                onClick={handleClick}
+                                className="user-avatar"
+                            />
+                            <Menu
+                                anchorEl={anchorEl}
+                                open={open}
+                                onClose={handleClose}
+                            >
+                                <MenuItem onClick={handleClose}>
+                                    <a href={`/profile/${user.role}`} className="menu-link">Profile</a>
+                                </MenuItem>
+                                <MenuItem onClick={handleClose}>
+                                    <a href="/" className="menu-link">Sign Out</a>
+                                </MenuItem>
+                            </Menu>
+                        </div>
 
                 ) : (
-                    <div className="user-hi">
+                    <div className="navbar-guest">
                         <Button
                             className="log-in"
                             variant="contained"
