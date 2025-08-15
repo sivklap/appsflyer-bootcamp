dcb4999da755e9a403f750786e10d04837894b12
yaelshtaiman <yaelshtaiman@gmail.com>
2025-08-15 12:23:27 +0300

    mentee profile

--- client/src/pages/ProfileMentee.js
+++ client/src/pages/ProfileMentee.js
@@ -2,9 +2,31 @@ import React from "react"
 import "./ProfileMentee.css"
 
 const ProfileMentee = ({user}) => {
+    if (!user){
+        return <p>Please log in to view this profile.</p>;
+    }
+    if(user.role === 'mentor'){
+        return <p>You have to be a mentee to see this</p>
+    }
     return (
-        <div>
-            Profile Mentee page
+        <div className="profile-mentee-page">
+            <div className="profile-mentee-header">
+                <img
+                    src={`/images/avatars/avatar-${user.img}.png`}
+                    alt={user.first_name}
+                    className="profile-mentee-avatar"
+                />
+                <h1>{user.first_name}  {user.last_name}</h1>
+                <p className="profile-mentee-role">{user.role}</p>
+            </div>
+            <div className="profile-mentee-info">
+                <h2>Contact Information</h2>
+                <p><b>Email:</b> {user.email}</p>
+                <p><b>Phone: </b>{user.phone_number}</p>
+
+                <h2>Professional Info</h2>
+                <p><b>Bio: </b> {user.bio}</p>
+            </div>
         </div>
     )
 }
