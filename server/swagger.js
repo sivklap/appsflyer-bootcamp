ab0b3c5eb841df369231209763389d963d9b4021
sivank <sivan.klapholz@gmail.com>
2025-08-15 18:09:03 +0300

    made signup work

--- server/swagger.js
+++ server/swagger.js
@@ -144,7 +144,7 @@ const swaggerDocument = {
           },
           password: {
             type: 'string',
-            description: 'User password',
+            description: 'User password (will be hashed)',
             example: 'password123'
           },
           role: {
