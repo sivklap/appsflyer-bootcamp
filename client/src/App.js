ae993f2587ec9a06a763623408320f5d7fc183ca
yaelshtaiman <yaelshtaiman@gmail.com>
2025-08-19 11:35:14 +0300

    cleanup

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,11 @@
 import React, {useState, useEffect} from "react";
+import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-// import MenteeRegistrationPage from "./pages/MenteeRegistrationPage";
-// import MentorRegistrationPage from "./pages/MentorRegistrationPage";
 import MentorsPage from "./pages/MentorsPage";
 import NavBar from "./components/NavBar";
 import LoginForm from "./components/auth/LoginForm";
 import SignupForm from "./components/auth/SignupForm";
-
 import WelcomePage from "./pages/WelcomePage";
-import "./App.css";
 import ProfileMentor from "./pages/ProfileMentor";
 import ProfileMentee from "./pages/ProfileMentee";
 import MentorHomePage from "./pages/MentorHomePage";
@@ -41,8 +38,6 @@ function App() {
             <Route path="/signup" element={<SignupForm availableLanguages={availableLanguages} />} />
 
             <Route path="/mentors" element={<MentorsPage user={user} availableLanguages={availableLanguages} />} />
-            {/*<Route path="/register/mentor" element={<MentorRegistrationPage />} />*/}
-            {/*<Route path="/register/mentee" element={<MenteeRegistrationPage />} />*/}
             <Route path="/profile/mentor" element={<ProfileMentor user={user} setUser={setUser} />} />
             <Route path="/profile/mentee" element={<ProfileMentee user={user}/>} />
             <Route path="/mentor-home" element={<MentorHomePage user={user}/>} />
